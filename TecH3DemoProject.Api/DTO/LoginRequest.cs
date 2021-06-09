d41bb833b72f631a0614091356f4ef49d2e38db5
Jack Baltzer <jabb@tec.dk>
2021-06-09 10:44:03 +0200

    jwt tests

---
+++ TecH3DemoProject.Api/DTO/LoginRequest.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TecH3DemoProject.Api.DTO
+{
+    public class LoginRequest
+    {
+        [Required]
+        [StringLength(128, ErrorMessage = "Email length can't be more than 128 chars")]
+        public string Email { get; set; }
+
+        [JsonIgnore]
+        [Required]
+        [MinLength(8, ErrorMessage = "Password must be 8 or more chars")]
+        [StringLength(64, ErrorMessage = "Password length can't be more than 64 chars")]
+        public string Password { get; set; }
+    }
+}
---
+++ TecH3DemoProject.Api/Domain/Login.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+
+namespace TecH3DemoProject.Api.Domain
+{
+    public class Login : BaseModel
+    {
+        [Required]
+        [StringLength(128, ErrorMessage = "Email length can't be more than 128 chars")]
+        public string Email { get; set; }
+
+        [JsonIgnore]
+        [Required]
+        [MinLength(8, ErrorMessage = "Password must be 8 or more chars")]
+        [StringLength(64, ErrorMessage = "Password length can't be more than 64 chars")]
+        public string Password { get; set; }
+
+        [Required]
+        [Range(0,100, ErrorMessage ="AccessLevel must be between 0 and 100")]
+        public int AccessLevel { get; set; } // 0 = gæst, 1 = kunde, 100 = admin
+    }
+}
